b42a7cb638b31e56680719fe7fd1a0e3dd8de297
liyy <785618204@qq.com>
2019-04-07 23:54:24 +0800

    'common'

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
+const request = require('request')
+
+const serverUrl='http://yyxyapi.herokuapp.com';//server地址
 
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use('/', function(req, res) {
+	let url = serverUrl + req.url;
+	req.pipe(request(url)).pipe(res);
+});
 
 var port = process.env.PORT || 5000
 app.listen(port)
